597a1b5891f2a625088198ff47c51b4b9a4fb4df
219902 <219902@student.agh.edu.pl>
2025-01-09 17:09:49 +0100

    tb for risign edge counter cleanup

--- TC_7LED.srcs/sim_1/new/tb_rising_edge_counter.v
+++ TC_7LED.srcs/sim_1/new/tb_rising_edge_counter.v
@@ -32,6 +32,7 @@ wire [3:0] OUT_2;
 wire [3:0] OUT_3;
 wire [15:0] OUT;
 
+assign { OUT_3, OUT_2, OUT_1, OUT_0 } = OUT;
 //module rising_edge_counter(input clk, input in, input reset, output reg [15:0] out);
 rising_edge_counter uut(.clk(clk), .in(IN), .reset(RESET), .out(OUT));
 
@@ -116,8 +117,6 @@ initial begin
 #100
 	RESET = 1'b1;
 	IN = 1'b0;
-#50
-	RESET = 1'b0;
 
 #100
 	IN = 1'b1;
